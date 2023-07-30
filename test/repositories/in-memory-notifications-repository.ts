6ba159da768de24489bd965a86b91714c182da6b
GabrielaSinastre <gabrielamaria.gaby@hotmail.com>
2023-07-30 20:54:23 -0300

    in memory concepts and prisma settings

---
+++ test/repositories/in-memory-notifications-repository.ts
@@ -0,0 +1,13 @@
+import { Notification } from '../../src/application/entities/notification';
+import { NotificationsRepository } from '../../src/application/repositories/notifications-repository';
+
+//banco fake pra passar como parametro
+export class InMemoryNotificationsRepository
+  implements NotificationsRepository
+{
+  public notifications: Notification[] = [];
+
+  async create(notification: Notification) {
+    this.notifications.push(notification);
+  }
+}
